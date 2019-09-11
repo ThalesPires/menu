60c13940019d534fa053e15e79068710d51000ce
ThalesPires <55162978+ThalesPires@users.noreply.github.com>
2019-09-11 10:34:41 -0300

    Update menuex.cpp

--- menuex.cpp
+++ menuex.cpp
@@ -4,13 +4,7 @@
 int main(){
 	
 	int i;
-	int a;
-	float A;
-	int B;
-	int x;
-	int y;
-	double sqrt;
-	int resultado;
+	
 	do	
 	{
 		printf("Digite um numero\n0 - sair\n1 - verificar par\n"
@@ -19,19 +13,19 @@ int main(){
 		scanf("%d", &i);	
 		switch(i){
 			case 1:
+				int a;
         			printf("Digite um numero para vereficar se e par ou nao\n");
 				scanf("%d", &a);
-				printf("\n");
 				if(a % 2 == 0){
 					printf("este numero e par!\n");	
 				}
 				else{
 					printf("este numero e impar!\n");
 				}
-				printf("\n");
 				break;
 			case 2:
-				printf("\n");
+				float A;
+				int B, resultado;
 				printf("Digite o valor de A\n");
 				scanf("%f", &A);
 				if(A < 0){
@@ -46,17 +40,16 @@ int main(){
 				}
 				resultado = pow(A, B);
 				printf("O numero em notacao cientifica fica assim: %.2e\n",resultado);
-				printf("\n");
 				break;
 			case 3:
-				printf("\n");
+				int x, y;
+				double sqrt;;
 				printf("Digite o valor de A\n");
 				scanf("%f", &x);
 				printf("Digite o valor de B\n");
 				scanf("%f", &y);
 				sqrt = pow(x, (1.0/y));
 				printf("O numero e: %.2e\n",sqrt);
-				printf("\n");
 				break;
 			case 4:
 				printf("pastel muito bom!\n");
